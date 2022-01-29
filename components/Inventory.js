eb009406433c2d45e5b2fae1675c5f2587f5ef80
Mor <mortaob@gmail.com>
2022-01-29 14:27:56 +0200

    Order by champion stars.

--- components/Inventory.js
+++ components/Inventory.js
@@ -1,31 +1,31 @@
 import styles from './Inventory.module.scss';
+import { useMemo } from "react";
 
 const Inventory = ({ user }) => {
-
-
+  const sortInventory = (inventory) => {
+    return inventory?.sort((a, b) => b.level - a.level);
+  }
+  const inventory = useMemo(() => sortInventory(user?.inventory), [user]);
   return (
-      <div className={styles.inventoryContainer}>
-        {
-          user.inventory.map((item) => {
-            return (
-                <div className={styles.itemContainer} key={item.id}>
-                  <div>
-                    <h3 className={styles.itemDesc}>{item.name}</h3>
-                    <img
-                        className={styles.inventoryImg}
-                        src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${item.id}_0.jpg`}
-                        alt="Problem" />
-                    <p className={styles.itemDesc}> <strong>{item.level} {item.level >= 3 && '⭐'}</strong></p>
-                  </div>
-                </div>
-            );
-          })
-        }
-
-      </div>
-
+    <div className={styles.inventoryContainer}>
+      {
+        inventory?.map((item, index) => {
+          return (
+            <div className={styles.itemContainer} key={`${item.id}-${index}`}>
+              <div>
+                <h3 className={styles.itemDesc}>{item.name}</h3>
+                <img
+                  className={styles.inventoryImg}
+                  src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${item.id}_0.jpg`}
+                  alt="Problem"/>
+                <p className={styles.itemDesc}><strong>{item.level} {item.level >= 3 && '⭐'}</strong></p>
+              </div>
+            </div>
+          );
+        })
+      }
+    </div>
   );
-
 };
 
 export default Inventory;
