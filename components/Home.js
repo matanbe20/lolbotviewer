adae754d190a596d63f4be5297014cc236a7814a
ZivCarmi <carmi.ziv@gmail.com>
2023-06-23 12:25:33 +0300

    new podium & next links

--- components/Home.js
+++ components/Home.js
@@ -1,58 +1,104 @@
-import styles from './Home.module.css';
-import { useMediaQuery } from './hooks/useMediaQuery';
-import { useMemo } from 'react';
+import Link from "next/link";
+import styles from "./Home.module.css";
+import { useMediaQuery } from "./hooks/useMediaQuery";
+import { useMemo } from "react";
+import Image from "next/image";
 
 const medals = {
-  0: '🏅', 1: '🥈', 2: '🥉',
-}
+  0: "🏅",
+  1: "🥈",
+  2: "🥉",
+};
 
 const Home = (props) => {
   if (typeof window === undefined) {
     return null;
   }
-  const isSmall = useMediaQuery('(max-width: 768px)');
+  const isSmall = useMediaQuery("(max-width: 768px)");
   const sorted = Object.keys(props.users).sort((a, b) => {
-    return props.users[b].inventory.length - props.users[a].inventory.length
+    return props.users[b].inventory.length - props.users[a].inventory.length;
   });
-  const goToInventory = userName => {
-    window.location.href = `/inventory/?user=${userName}`;
-  }
 
   const usersList = useMemo(() => {
     return !isSmall ? sorted.slice(3) : sorted;
   }, [isSmall]);
 
-  return <>
-    {!isSmall ? <div className={styles.top3}>
-      {sorted.slice(0, 3).map((value, i) => {
-        return <div key={i} onClick={() => goToInventory(value)}>
-          <div className={`${styles.topRank} ${i === 0 && styles.rank1} ${i === 1 && styles.rank2}`}>
-            <img style={{ borderRadius: '50%' }} height={50} src={props.users[value].avatarUrl ?? ''}
-                 alt=""/>
-            <span>{props.users[value].username}</span>
-            <div style={{ textAlign: 'center', marginBottom: 5 }}>{medals[i]}</div>
-            <p style={{ marginTop: 'auto' }}>{props.users[value].inventory.length} Champions</p>
+  return (
+    <>
+      {!isSmall ? (
+        <div className={styles.top3}>
+          {sorted.slice(0, 3).map((value, i) => (
+            <div
+              key={value}
+              className={`${styles.topRank} ${
+                i === 0
+                  ? styles.rank1
+                  : i === 1
+                  ? styles.rank2
+                  : i === 2
+                  ? styles.rank3
+                  : ""
+              }`}
+            >
+              <Link
+                href={`/inventory/?user=${value}`}
+                as={`/inventory/${value}`}
+              >
+                <img
+                  style={{ borderRadius: "50%" }}
+                  height={50}
+                  src={props.users[value].avatarUrl ?? ""}
+                  alt=""
+                />
+                <span>{props.users[value].username}</span>
+                <div style={{ textAlign: "center", marginBottom: 5 }}>
+                  {medals[i]}
+                </div>
+                <p style={{ marginTop: "auto" }}>
+                  {props.users[value].inventory.length} Champions
+                </p>
+              </Link>
+              <div className={styles.podium}>
+                <div className={styles.medalWrapper}>
+                  <Image
+                    src={`/podium${i + 1}.png`}
+                    alt={i + 1}
+                    width={70}
+                    height={70}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : null}
+      <div className={styles.leaderboard}>
+        {usersList.map((value, i) => (
+          <div
+            className={styles.leaderboardRow}
+            key={value}
+            style={{
+              backgroundColor: i % 2 === 0 ? "" : "#acacac0f",
+              width: isSmall ? "100%" : 600,
+            }}
+          >
+            <Link href={`/inventory/?user=${value}`} as={`/inventory/${value}`}>
+              <img
+                style={{ borderRadius: "50%" }}
+                height={50}
+                src={props.users[value].avatarUrl}
+                alt=""
+              />
+              <span>{props.users[value].username ?? "Unknown"}</span>
+              <span className={styles.champions}>
+                {props.users[value].inventory.length} champions
+              </span>
+            </Link>
           </div>
-        </div>
-      })}
-    </div> : null}
-    <div className={styles.leaderboard}>
-      {usersList.map((value, i) => {
-        return <div onClick={() => goToInventory(value)}
-                    className={styles.leaderboardRow}
-                    key={'leaderboard-' + i}
-                    style={{
-                      backgroundColor: i % 2 === 0 ? '' : '#acacac0f',
-                      width: isSmall ? '100%' : 500
-                    }}>
-          <img style={{ borderRadius: '50%' }} height={50} src={props.users[value].avatarUrl}
-               alt=""/>
-          <span>{props.users[value].username ?? 'Unknown'}</span>
-          <span className={styles.champions}>{props.users[value].inventory.length} champions</span>
-        </div>
-      })}
-    </div>
-  </>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- pages/inventory/index.js
+++ pages/inventory/index.js
@@ -1,27 +1,40 @@
-import styles from '../../styles/Home.module.css';
-import Inventory from '../../components/Inventory';
+import styles from "../../styles/Home.module.css";
+import Inventory from "../../components/Inventory";
+import Head from "next/head";
 
 export default function Home(props) {
   let totalLevels = 0;
-  props.user.inventory.forEach(item => {
-    totalLevels += parseInt(item.level)
+  props.user.inventory.forEach((item) => {
+    totalLevels += parseInt(item.level);
   });
 
-  console.log(totalLevels)
+  console.log(totalLevels);
   return (
-      <div className={styles.container}>
-        <h2 className={styles.main_title}><span className={styles.userName}> {props.user.username}</span>, you got a total of <span className={styles.number_champs}>{props.user.inventory.length}</span> Champions</h2>
-        <h4 style={{textAlign: 'center'}}>Total levels: {totalLevels}</h4>
-        <Inventory user={props.user}/>
-      </div>
+    <div className={styles.container}>
+      <Head>
+        <title>{props.user.username ?? "Unknown"}'s inventory</title>
+      </Head>
+      <h2 className={styles.main_title}>
+        <span className={styles.userName}> {props.user.username}</span>, you got
+        a total of{" "}
+        <span className={styles.number_champs}>
+          {props.user.inventory.length}
+        </span>{" "}
+        Champions
+      </h2>
+      <h4 style={{ textAlign: "center" }}>Total levels: {totalLevels}</h4>
+      <Inventory user={props.user} />
+    </div>
   );
 }
 
 Home.getInitialProps = async (ctx) => {
   console.log(ctx.query.user);
-  const baseUrl = process.env.node_env === 'production' ? 'https://lolbotviewer.vercel.app' : 'http://localhost:3000';
-  const res = await fetch(
-      `${baseUrl}/api/fetch?user=${ctx.query.user}`);
+  const baseUrl =
+    process.env.node_env === "production"
+      ? "https://lolbotviewer.vercel.app"
+      : "http://localhost:3000";
+  const res = await fetch(`${baseUrl}/api/fetch?user=${ctx.query.user}`);
   const result = await res.json();
   return {
     user: result,
